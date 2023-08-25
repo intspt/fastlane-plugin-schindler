3814751110eb0b209f0ed3ca0e1ab7ea997fdea9
狗木 <goumu.lb@alibaba-inc.com>
2023-08-25 14:22:22 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-/usr/bin/gem build fastlane-plugin-schindler.gemspec
-sudo /usr/bin/gem uninstall fastlane-plugin-schindler -a -x
-sudo /usr/bin/gem install fastlane-plugin-schindler-*.gem -n /usr/local/bin
+gem build fastlane-plugin-schindler.gemspec
+gem uninstall fastlane-plugin-schindler -a -x
+gem install fastlane-plugin-schindler-*.gem --user-install -n /usr/local/bin
 rm fastlane-plugin-schindler-*.gem
